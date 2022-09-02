a12ae9e9bcc0234180c2f79ab50258690400834b
sako <2511013095@qq.com>
2022-09-02 17:53:18 +0800

    lab1

--- .history/lab1/LeapYear_20220902175106.java
+++ .history/lab1/LeapYear_20220902175106.java
@@ -6,7 +6,7 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
-    public static boolean isLeapYear(int year) {
+    public static void isLeapYear(int year) {
         return ((year%4==0&&year%100!=0)||year%400==0);
     }
     private static void checkLeapYear(int year) {
